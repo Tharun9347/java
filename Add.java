d72bd70797af3e47a53a3223836d21328c6b25c5
Tharun9347 <143396950+Tharun9347@users.noreply.github.com>
2023-09-06 16:28:12 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -9,5 +9,24 @@ sum=i+sum;
 i--;
 }
 System.out.println(sum);
+public class Seven_table {
+
+	public static void main(String[] args) 
+	{
+	  int n=7,i=1,num;
+	  for(i=1;i<=10;i++)
+	  {
+		  num=n*i;
+		  if(i%2==0)
+		  {
+			  System.out.println(n+"*"+i+"="+num);
+			  
+		  }
+	  }
+
+	}
+
+}
+
 }
 }
